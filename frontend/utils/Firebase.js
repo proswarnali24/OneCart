bea99021aad3690a232676dff644b0be9b9f0951
Sornali_Sen <sornalisen18@gmail.com>
2026-01-10 17:08:07 +0530

    Update Firebase configuration for new project

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -2,11 +2,11 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY ,
-  authDomain: "loginonecart.firebaseapp.com",
-  projectId: "loginonecart",
-  storageBucket: "loginonecart.firebasestorage.app",
-  messagingSenderId: "242165258894",
-  appId: "1:242165258894:web:0155a2ced93e20073247df"
+  authDomain: "onecartlogin-53a75.firebaseapp.com",
+  projectId: "onecartlogin-53a75",
+  storageBucket: "onecartlogin-53a75.firebasestorage.app",
+  messagingSenderId: "1075733983146",
+  appId: "1:1075733983146:web:6f147690aa598b5a9b85d3"
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
